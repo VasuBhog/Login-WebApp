f120ba4841729e046ffb17e464f86aae0504796d
Vasu Bhog <bhogvu@mail.uc.edu>
2019-09-18 22:35:41 -0400

    Completed all navigation from Login, Register, Homepage and Logout

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, Response, session, abort
 from flask_caching import Cache
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 cache = Cache(app)
+# db = SQLAlchemy(app)
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     email = db.Column(db.String, unique=True, nullable=False)
+
+# db.session.commit()
+
+#Reroutes to home authentication
 @app.route("/")
+def index():
+    return home()
+
+#HomePage
+#Starts here with checking if user logs in
+@app.route("/home",methods=['POST'])
 def home():
+    print(session.get('logged_in'))
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
+        if request.method == 'POST':
+            if request.form['action'] == 'Logout':
+                    session['logged_in'] = False
+                    print(session.get('logged_in'))
+                    return render_template('login.html')
         return render_template("home.html")
 
 
-@app.route("/login", methods=['GET','POST'])
+#Login Page
+#Checks to see which button the user presses
+@app.route("/login", methods=['POST'])
 def login():
     if request.method == 'POST':
         if request.form['action'] == 'Login':
@@ -25,16 +49,15 @@ def login():
             return home()
         if request.form['action'] == 'Register':
             return register()
-@app.route("/register")
+
+#Registration Form
+@app.route("/register",methods=['POST'])
 def register():
+    if request.method == 'POST':
+        if request.form['action'] == 'Submit':
+            session['logged_in'] = True
+            return home()
     return render_template("register.html")
-
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return home()
-
     
 if __name__ == "__main__":
     cache.clear()
