39f83bbce1a2d5aaa133d1c11a995d684760b063
Vasu Bhog <bhogvu@mail.uc.edu>
2019-09-20 15:59:42 -0400

    Fixed database issue, login page and registration verifcation

---
+++ WebDb.sql
@@ -0,0 +1,3 @@
+SET SQL_SAFE_UPDATES = 0;
+Select * from `users`.`user`;
+DELETE FROM `users`.`user`;
--- app.py
+++ app.py
@@ -8,38 +8,14 @@ import os
 app = Flask(__name__)
 app.debug = True
 app.secret_key = os.urandom(12)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:cookie123@localhost/users'
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     username = db.Column(db.String, primary_key=True)
-#     password = db.Column(db.String, unique=True, nullable=False)
-#     firstname = db.Column(db.String, unique=True, nullable=False)
-#     lastname = db.Column(db.String, unique=True, nullable=False)
-#     email = db.Column(db.String, unique=True, nullable=False)
-#     authenticated = db.Column(db.Boolean, default=False)
-
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-
 
 #Connect to mysql
 mydb = mysql.connector.connect(host='localhost', user='root',password='cookie123', database='users')
-mycursor = mydb.cursor()
+mydb.autocommit = True
+cursor = mydb.cursor(buffered=True)
 
-def insert_user(username, password, firstname,lastname,email):
-    
-    query = "INSERT INTO user(username,password,firstname,lastname,email) " \
-            "VALUES(%s,%s,%s,%s,%s)"
-    args = (username, password, firstname,lastname,email)
-    mycursor.execute(query,args)
-    mydb.commit()
-    # mycursor.close()
+#Queries
+user_query = ('SELECT * FROM user WHERE username = %s AND password = %s')
 
 #Reroutes to home authentication
 @app.route("/")
@@ -50,6 +26,7 @@ def index():
 #Starts here with checking if user logs in
 @app.route("/home",methods=['GET','POST'])
 def home():
+    msg =''
     print(session.get('logged_in'))
     #Checks Logged in session
     if not session.get('logged_in'):
@@ -57,28 +34,38 @@ def home():
     #logged in or registered
     else:
         if request.method == 'POST' and 'username' in session:
-            print("WHHUYYYY")
+            print("START SESSION")
             username = session['username']
             password = session['password']
-            mycursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password))
-            account = mycursor.fetchone()
-            print(account)
+            cursor.execute(user_query, (username, password))
+            account = cursor.fetchone()
             if account:
+                print("2")
                 username = account[0]
                 password = account[1]
                 firstname = account[2]
                 lastname = account[3]
                 email = account[4]
                 return render_template("home.html",firstname=firstname,lastname=lastname,email=email)
-            
-        elif request.method == 'POST' and request.form['action'] == 'Logout':
+        elif request.form['action'] == 'Logout':
+            print("LOGGOUTTT")
+            close_session()
             session['logged_in'] = False
             session.pop('logged_in',None)
             session.pop('username',None)
             session.pop('password',None)
-            return home()
+            msg="You have been logged out"
+            return render_template("login.html",msg=msg)
         return render_template("home.html", firstname = session['firstname'], lastname = session['lastname'], email = session['email'])
+    return home()
 
+# @app.route("/logout",methods=["GET"])
+# @login_required
+# def logout():
+#     user = current_user
+#     user.authe
+def close_session():
+    [session.pop(key) for key in list(session.keys())]
 
 #Login Page
 #Checks to see which button the user presses
@@ -88,65 +75,63 @@ def login():
     """ GET - display
         POST - login """
     if request.method == 'POST' and request.form['action'] == 'Login' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        session['username'] = username
-        session['password'] = password
-        mycursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password))
-        account = mycursor.fetchone()
+        userdetails = request.form
+        username = userdetails['username']
+        password = userdetails['password']
+        args = (username,password)
+        
+        cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', args)
+        account = cursor.fetchone()
         if account:
+            session['username'] = username
+            session['password'] = password
             session['logged_in'] = True
             return home()
         else:
+            print("TESTSETSETSTTSETESTSE")
             msg = 'Incorrect Username/Password'
+            session['logged_in'] = True
             render_template('login.html',msg=msg)
     if request.method == 'POST' and request.form['action'] == 'Register':
-        return register()
+        return render_template("register.html")
 
 #Registration Form
 @app.route("/register",methods=['GET','POST'])
 def register():
-    if request.method == 'POST':
-        if request.form['action'] == 'Submit':
-            username = request.form['username']
-            password = request.form['password']
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            email = request.form['email']
-            mycursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password))
-            account = mycursor.fetchone()
-            if account[0] == username:
-                return "Username already used"
-            else:
-                insert_user(username, password, firstname,lastname,email)
+        if request.method == 'POST' and request.form['action'] == 'Submit':
+            msg = ''
+            userdetails = request.form
+            username = userdetails['username']
+            password = userdetails['password']
+            args = (username,password)
+            cursor.execute(user_query,args)
+            result = cursor.fetchone()
+            if result:
+                msg = "Username already exists"
+                return render_template('register.html',msg=msg)
+            else: 
+                firstname = userdetails['firstname']
+                lastname = userdetails['lastname']
+                email = userdetails['email']
                 session['username'] = username
                 session['password'] = password
                 session['firstname'] = firstname
                 session['lastname'] = lastname
                 session['email'] = email
                 session['logged_in'] = True
-                return home()
-    return render_template("register.html")
-    
+                insert_user(username, password, firstname,lastname,email)
+                return render_template("home.html", firstname = session['firstname'], lastname = session['lastname'], email = session['email'])
+            return home()
+
+#insert users to table
+def insert_user(username, password, firstname,lastname,email):
+    query = "INSERT INTO user(username,password,firstname,lastname,email) " \
+            "VALUES(%s,%s,%s,%s,%s)"
+    args = (username, password, firstname,lastname,email)
+    cursor.execute(query,args)
+    mydb.commit()
+    # cursor.close()
+
 if __name__ == "__main__":
     app.run()
 
-    # db = SQLAlchemy()
-
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     username = db.Column(db.String, primary_key=True)
-#     password = db.Column(db.String, unique=True, nullable=False)
-#     firstname = db.Column(db.String, unique=True, nullable=False)
-#     lastname = db.Column(db.String, unique=True, nullable=False)
-#     email = db.Column(db.String, unique=True, nullable=False)
-#     authenticated = db.Column(db.Boolean, default=False)
-
-#     def is_active(self):
-#         return True
-#     def get_id(self):
-#         return self.username
-#     def is_authenticated(self):
-#         return self.authenticated
-#     def is_anonymous(self):
-#         return False
