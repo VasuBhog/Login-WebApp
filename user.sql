600f76353df52dcf34eb190e89da63ae4b80de72
Vasu Bhog <bhogvu@mail.uc.edu>
2019-09-22 22:13:14 -0400

    Database update

--- user.sql
+++ user.sql
@@ -4,5 +4,11 @@ CREATE TABLE `user` (
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `wordcount` int(11) DEFAULT NULL,
   PRIMARY KEY (`username`)
-)
+);
+
+ALTER TABLE `users`.`user` 
+ADD COLUMN `file` VARCHAR(255) NULL AFTER `email`,
+ADD COLUMN `wordcount` INT NULL AFTER `file`;
